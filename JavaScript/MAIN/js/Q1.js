a56ab40af8498993c6aba8a5e106acca957bc5d8
ravi-prakash1907 <1907prakash.ravi@gmail.com>
2019-09-20 20:50:28 +0530

    Q1

--- JavaScript/MAIN/js/Q1.js
+++ JavaScript/MAIN/js/Q1.js
@@ -7,12 +7,43 @@ function varify() {
   if(name == "" || rollno == "" || dob == ""){
     alert("Fill it!");
   } else {
-    checkName(name);
+    //checkName(name);
+    //checkRoll(rollno);
+    checkDOB(dob);
   }
 }
 
 function checkName(x) {
-  var t = x.match(/^[a-z]/g);
-  //if(t != null)
-    alert(t)
+  var t = x.match(/[^a-z|A-Z|\s]+/g);
+  if(t !== null)
+    alert('Name is not valid.')
+}
+
+function checkRoll(x) {
+  if(x.length !== 7)
+    alert('Roll no. is not valid.')
+}
+
+function checkDOB(x) {
+  var t = x.match(/[0-3][0-9][/][01][0-9][/][0-2][0-9][0-9][0-9]/);
+  if(t === null){
+    alert('DOB is not valid.')
+  } else {
+
+    var date = x.slice(0,2);
+    var m = x.slice(3,5);
+    var year = x.slice(6);
+
+    x = m+"/"+date+"/"+year;
+
+
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    var d = new Date(x);
+    var day = weekday[d.getDay()];
+    var month = months[d.getMonth()];
+
+    alert("Your Date of Birth:\n\n" + day + "\n" + month+ " " + date + ", " + year);
+  }
 }
